850d6a0256e55972676062fec79378e481785688
DouglasMonteles <silvadouglas328@gmail.com>
2020-05-20 15:45:08 -0300

    Tratando erros 403

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,32 +1,46 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
+import { StorageService } from '../services/storage.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // console.log("Passou no interceptor");
-        return next.handle(req)
-          .catch((error, caught) => {
-              let errorObj = error;
-              if (errorObj.error) {
-                  errorObj = errorObj.error;
-              }
-              if (!errorObj.status) {
-                  errorObj = JSON.parse(errorObj);
-              }
+  constructor(public storageService: StorageService) {}
 
-              console.log("Erro detectado pelo interceptor:");
-              console.log(errorObj);
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      // console.log("Passou no interceptor");
+      return next.handle(req)
+        .catch((error, caught) => {
 
-              return Observable.throw(errorObj);
-          }) as any;
-    }
+            let errorObj = error;
+            if (errorObj.error) {
+                errorObj = errorObj.error;
+            }
+            if (!errorObj.status) {
+                errorObj = JSON.parse(errorObj);
+            }
+
+            console.log("Erro detectado pelo interceptor:");
+            console.log(errorObj);
+
+            switch(errorObj.status) {
+              case 403:
+                this.handle403();
+              break;
+            }
+
+            return Observable.throw(errorObj);
+        }) as any;
+  }
+
+  handle403() {
+    this.storageService.setLocalUser(null);
+  }
 }
 
 export const ErrorInterceptorProvider = {
-    provide: HTTP_INTERCEPTORS,
-    useClass: ErrorInterceptor,
-    multi: true,
+  provide: HTTP_INTERCEPTORS,
+  useClass: ErrorInterceptor,
+  multi: true,
 };
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -33,9 +33,13 @@ export class ProfilePage {
           },
 
           (error) => {
-
+            if (error.status === 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
           }
         );
+    } else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
